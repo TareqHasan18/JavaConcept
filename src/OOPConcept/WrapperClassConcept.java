2060e47bffd1d6ce2397e30310e9f527aa63805d
AKM Tareq Hasan <tareqhasan@AKMs-MacBook-Pro.local>
2020-03-15 09:42:00 -0400

    Updated WrappernClassConcept

--- src/OOPConcept/WrapperClassConcept.java
+++ src/OOPConcept/WrapperClassConcept.java
@@ -5,5 +5,29 @@ public class WrapperClassConcept {
 
     public static void main(String[] args) {
 
+        String s = "120";
+        System.out.println(s+30);
+
+        //data conversion from String to int
+        int i = Integer.parseInt(s);
+        System.out.println(i + 30);
+
+        //String to double conversion
+        String t = "49.99";
+        double d = Double.parseDouble(t);
+        System.out.println(d + 10);
+
+        //String to boolean
+        String x = "true";
+        boolean b = Boolean.parseBoolean(x);
+        System.out.println(b);
+
+        //int to String conversion
+        int j = 200;
+        System.out.println(j + 20);
+
+        String k = String.valueOf(j);  //"200"
+        System.out.println(k + 20);  //this will print 20020
+
     }
 }
